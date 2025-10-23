8d54dab4000b67ff0cbd98d4951a7ef7fc6a1dcd
tokec <fleatoke@gmail.com>
2025-10-23 19:26:01 +0900

    Refactor Next.js contents page to enhance navigation with collapsible sections and update links for React Fundations. Rename component for clarity and implement useState for dynamic content display.

--- app/next-js-contents/page.tsx
+++ app/next-js-contents/page.tsx
@@ -1,17 +1,62 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import BackToHome from "../components/BackToHome";
 
-export default function Reacts() {
+
+
+export default function NextJSContents() {
+  const [isOpen, setIsOpen] =useState(false);
     return (
       <div className="container ">
         <h1 className="text-4xl font-bold mt-8">Next.js</h1>
         <p className="font-mono mb-4">Next.js is a framework for building server-side rendered React applications.</p>
-        <nav>
-            <ul className="text-2xl font-bold space-y-2">
+        {/* JS only */}
+        <details>
+            <summary className="text-2xl font-bold space-y-2">
+              React Fundations
+              <Link href="/next-js-contents/react-fundations" className="hover:underline">React Fundations</Link>
+            </summary>    
+
+            <ul className="font-mono space-y-2">
                 <li>
-                    <Link href="/next-js-contents/your-first-component" className="hover:underline">Your First Component</Link>
+                    <Link href="/next-js-contents/react-fundations" className="hover:underline">React Fundations</Link>
+                </li>
+                <li>
+                    <Link href="/next-js-contents/updating-ui-with-javascript" className="hover:underline">Upating UI with JavaScript</Link>
                 </li>
             </ul>
+        </details>
+
+        <div className="mt-8">
+          --------------------------------------------------------------
+        </div>
+        {/* useState custom */}
+        <nav>
+          <div>
+            <button 
+              onClick={() => setIsOpen(!isOpen)}
+              className="text-2xl font-bold space-y-2 hover:underline"
+              >
+                {isOpen ? "▼" : "▶"} React Fundations 
+              </button>
+
+              {isOpen && (
+                <ul className="ml-6 mt-2 space-y-2">
+                  <li>
+                    <Link href="/next-js-contents/react-fundatins" className="hover:underline">
+                    Chapter 1
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/next-js-contents/react-fundatins" className="hover:underline">
+                    Chapter 2
+                    </Link>
+                  </li>
+                </ul>
+              )}
+          </div>
         </nav>
         <BackToHome />
     </div>
